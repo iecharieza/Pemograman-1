51f2231d1c0610c5afe3a8ca1264df5167efa381
Rieza Devi Anggraini <icindesgraf@yahoo.com>
2011-11-03 20:40:52 +0700

    sesi07

---
+++ Praktikum/sesi-07/soal1/Service/CalculatorService.java
@@ -0,0 +1,9 @@
+package Service;
+
+public interface CalculatorService {
+	public Integer tambah (int bil1, int bil2) ;
+	public Integer kurang (int bil1, int bil2) ;
+	public Integer kali (int bil1, int bil2) ;
+	public Integer bagi (int bil1, int bil2) ;
+	
+}
