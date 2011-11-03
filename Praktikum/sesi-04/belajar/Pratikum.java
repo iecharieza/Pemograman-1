1332305ebe5d08dae5f33a0ffa1ee3bee06bd333
Rieza Devi Anggraini <icindesgraf@yahoo.com>
2011-11-03 20:39:51 +0700

    sesi 04

---
+++ Praktikum/sesi-04/belajar/Pratikum.java
@@ -0,0 +1,7 @@
+package belajar ;
+
+public class Pratikum {
+	public static void main (String [] xx){
+		System.out.println ("Pratikum 2") ;
+	}
+}
---
+++ Praktikum/sesi-04/halo.java
@@ -0,0 +1,5 @@
+public class halo {
+	public static void main (String [] xx) {
+		System.out.println ("Halo") ;
+	}
+}
