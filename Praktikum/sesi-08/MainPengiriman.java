61d1c8084e513907ecc1329843e0e6099fed6d14
rieza devi anggraini <riezaiecha@yahoo.co.id>
2012-01-28 20:25:13 +0700

    tambahan

---
+++ Praktikum/sesi-08/MainPengiriman.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class MainPengiriman extends Paket {
+	public MainPengiriman () {
+	
+	}
+	
+	public static void main(String args[]) {
+		MainPengiriman elie = new MainPengiriman () ;
+		
+		String nmPengirim = JOptionPane.showInputDialog
+		("Biaya Pengiriman Paket" + "\n Inputkan Nama Pengirim ");
+		
+		elie.setNmPengirim(nmPengirim);
+		
+		String tujuanPaket = JObtionPane.showInputDialog
+		("Biaya Pengiriman Paket" +"\n Inputkan Tujuan paket : Bali | Surabaya | Jakarta | Bandung");
+
+		elie.setTujuanPaket(tujuanPaket);
+		
+		String br = JOptionPane.showInputDialog
+		("Biaya Pengiriman Paket" + "\n Inputkan Berat paket :") ;
+		
+		int beratPaket = Integer.parseInt (br) ;
+		elie.setBeratPaket (beratPaket) ;
+		elie.biayaPerKg();
+		elie.Bayar ();
+		
+		JOptionPane.showMessageDialog( null,"Biaya Pengiriman Paket Adalah" + "\n Nama Pengirim :
+		" + elie.getNmPengirim () +"\n Tujuan paket :"
+		+ elie.getTujuanPaket() + "\n Berat paket : " +
+		elie.getBeratPaket () + " Kg" + "\n Biaya Per Kg
+		: " + elie.biayaPerKg () + "\n Total Bayar :" +
+		elie.Bayar () + "\nTerima Kasih... . . ^_^ " ) ;
+	}
+		
